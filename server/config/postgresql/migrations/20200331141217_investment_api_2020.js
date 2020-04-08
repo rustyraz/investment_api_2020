e1390589a040a0f7ec8ccdee56a53b90726e8c4b
Peter Waweru <pngesh@gmail.com>
2020-04-08 11:07:04 +0300

    updated the migration for the users

--- server/config/postgresql/migrations/20200331141217_investment_api_2020.js
+++ server/config/postgresql/migrations/20200331141217_investment_api_2020.js
@@ -4,6 +4,8 @@ exports.up = function(knex) {
       table.text('email').notNullable().unique();
       table.text('password');
       table.text('name');
+      table.timestamp('created_at').defaultTo(knex.fn.now())
+      table.timestamp('updated_at').defaultTo(knex.fn.now())
   });
 };
 
